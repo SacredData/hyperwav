ec0202f1b06499d5798b67a314335333a4453de0
agrathwohl <andrew@storyboard.fm>
2022-01-16 16:10:07 -0600

    remove passthrough from constructor

--- index.js
+++ index.js
@@ -7,15 +7,24 @@ const WaveFile = require('wavefile').WaveFile
 
 class Wavecore {
   constructor(source, opts={core:null,storage:ram}) {
+    this.core = null
+    this.source = null
+    // Instantiate stream for appending WAV file data to hypercore
     if (source instanceof Source) source.open(() => this.source = source)
-    if (opts.core) this.core = core
+    // Assign to a hypercore provided via constructor arguments
+    if (opts.core instanceof Hypercore) this.core = core
+    // Declaring a specific storage supercedes defining a specific hypercore
     if (opts.storage) this.core = new Hypercore(opts.storage)
+    // If there is still no hypercore lets just make a sane default one
     if (!this.core) this.core = new Hypercore(ram)
-    this.pt = new PassThrough()
-    this.pt.on('data', (d) => this.core.append(d))
+    this.core.on('ready', () => console.log('core is ready!', this.core.keyPair))
   }
   async _toHypercore() {
     await this.core.ready()
+
+    const pt = new PassThrough()
+    pt.on('data', (d) => this.core.append(d))
+
     const rs = fs.createReadStream(this.source.pathname)
     rs.on('end', () => console.log(this.core))
 
@@ -29,7 +38,7 @@ class Wavecore {
       Object.assign({ chunkSize, cue, fmt, smpl, tags }, {}))
     )
 
-    rs.pipe(this.pt)
+    rs.pipe(pt)
   }
   async _audioBuffer() {
     return fs.readFileSync(this.source.pathname)
