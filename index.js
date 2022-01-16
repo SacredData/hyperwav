5ac95934ab6e0c9ee43c67d77a1e69158d6138cf
agrathwohl <andrew@storyboard.fm>
2022-01-16 12:53:09 -0600

    init

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-module.exports = {}
+const Hypercore = require('hypercore')
+const { Source } = require('@storyboard-fm/little-media-box')
+
+class Wavecore {
+  constructor(source) {
+    if (source instanceof Source) source.open(() => this.source = source)
+  }
+}
+
+module.exports = Wavecore
