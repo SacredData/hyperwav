914015689aadad3a8f55a155330df04d615ad59d
agrathwohl <andrew@storyboard.fm>
2022-01-16 13:13:37 -0600

    works prtty ok!

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Hypercore = require('hypercore')
 const { PassThrough } = require('stream')
 const ram = require('random-access-memory')
 const { Source } = require('@storyboard-fm/little-media-box')
+const WaveFile = require('wavefile').WaveFile
 
 class Wavecore {
   constructor(source) {
@@ -15,7 +16,19 @@ class Wavecore {
     const rs = fs.createReadStream(this.source.pathname)
     rs.on('end', () => console.log('reading ended'))
     rs.on('close', () => { return this.core })
+
+    // Get WAV metadata and headers for index 0 of our hypercore
+    const wavfile = new WaveFile()
+    wavfile.fromBuffer(await this._audioBuffer())
+    const { chunkSize, cue, fmt, smpl, tags } = wavfile
+    this.core.append(JSON.stringify(
+      Object.assign({ chunkSize, cue, fmt, smpl, tags }, {}))
+    )
+
     rs.pipe(this.pt)
+  }
+  async _audioBuffer() {
+    return fs.readFileSync(this.source.pathname)
   }
 }
 
