683cf9acbb755146357363ceaa117521bfa3b819
agrathwohl <andrew@storyboard.fm>
2022-01-18 10:45:52 -0600

    move test file to test dir; add some unit tests

---
+++ test/index.js
@@ -0,0 +1,35 @@
+const expect = require('chai').expect
+const Hypercore = require('hypercore')
+const path = require('path')
+const ram = require('random-access-memory')
+const { Source } = require('@storyboard-fm/little-media-box')
+const Wavecore = require('../')
+
+describe('Wavecore', function () {
+  describe('#constructor', function() {
+    const source = new Source(path.join(__dirname, 'test.wav'))
+    it('should return a new instance of a Wavecore', function () {
+      const core1 = new Wavecore({ source })
+      expect(core1).to.be.instanceof(Wavecore)
+    })
+    it('should work if no source is provided', () => {
+      const core2 = new Wavecore()
+      expect(core2).to.be.instanceof(Wavecore)
+    })
+    it('should work if a hypercore is provided as an argument', function () {
+      const hypercore = new Hypercore(ram)
+      const core3 = new Wavecore({ core: hypercore })
+      expect(core3).to.be.instanceof(Wavecore)
+    })
+  })
+  describe('#toHypercore', function() {
+    const source = new Source(path.join(__dirname, 'test.wav'))
+    it('should read the WAV into a new Hypercore', async function () {
+      const core4 = new Wavecore({ source })
+      const returnedCore = await Promise.resolve(core4.toHypercore())
+      expect(returnedCore).to.be.instanceof(Hypercore) &&
+        expect(core4.core).to.be.instanceof(Hypercore) &&
+        expect(core4.core.length).to.equal(68)
+    })
+  })
+})
