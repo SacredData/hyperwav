4e9df7f92434e025f839e2f4a4b92a599188a56c
agrathwohl <andrew@storyboard.fm>
2022-01-22 14:43:05 -0600

    add nanoprocess; add new template wav example

---
+++ examples/example.js
@@ -0,0 +1,31 @@
+const fs = require('fs')
+const path = require('path')
+const { Source } = require('@storyboard-fm/little-media-box')
+const Wavecore = require('.')
+
+const source = new Source(path.join(__dirname, 'test', 'test.wav'))
+source.open(async () => {
+  console.log('opened source WAV file', source)
+  async function main() {
+    const w = new Wavecore({ source })
+    console.log(w.core)
+    console.log('appending to hypercore...')
+    await w.toHypercore({loadSamples:true})
+    console.log('done', w.core)
+    console.log('lets cut it down to 12 sec or so')
+    await w.truncate(20)
+    await w.core.update()
+    console.log('done')
+    console.log('the hypercore is smaller now', w.core)
+    console.log('lets write the shorter file to disk...')
+    w._wavStream().pipe(
+      fs.createWriteStream('shorter-test.wav')
+      .on('close', () => console.log('done writing'))
+    )
+    return
+  }
+  await main()
+})
+  /*
+  }
+    */
---
+++ examples/example2.js
@@ -0,0 +1,14 @@
+const { Source } = require('@storyboard-fm/little-media-box')
+const Wavecore = require('.')
+
+const source = new Source('./test/test.wav')
+const w = new Wavecore({ source })
+
+async function main() {
+  await Promise.resolve(w.toHypercore())
+  console.log('splitting Wavecore at index 22....')
+  const [head, tail] = await Promise.resolve(w.split(22))
+  console.log('done!', head, tail)
+}
+
+main()
