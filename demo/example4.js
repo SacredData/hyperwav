8378c26cde938c4d06fb9e8b922115f6fead4e71
agrathwohl <andrew@storyboard.fm>
2022-02-16 11:46:17 -0600

    more accurate example4

--- demo/example4.js
+++ demo/example4.js
@@ -9,13 +9,16 @@ function silentOrVoice(data) {
   const zeros = samples.filter(d=>d==0)
   const zeroPercent = zeros.length / samples.length
 
-  if (zeroPercent < 0.1) return 'voice'
+  if (zeroPercent < 0.3) return 'voice'
   return 'silence'
 }
 
 async function main(num=0) {
-  const source = new Source('./clip.wav')
+  // const source = new Source('./clip.wav')
+  const source = new Source('/home/gwohl/Downloads/m.wav')
   const wavecore = new Wavecore({ source })
+
+  const INDEX_MS = 800
 
   await Promise.resolve(wavecore.open())
   console.log(wavecore.core)
@@ -27,7 +30,9 @@ async function main(num=0) {
     blocks.push(silentOrVoice(block))
   }
 
-  console.log(blocks)
+  let counter = 0
+  blocks.forEach(b=>console.log(`${counter * INDEX_MS} ${counter++} ${b}`))
+  //console.log(blocks)
 }
 
 main()
