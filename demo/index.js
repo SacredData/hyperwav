4df3dfa35db003ab2f1406bef65eb00de08550e8
agrathwohl <andrew@storyboard.fm>
2022-03-02 16:40:26 -0600

    enable listening to original non-processed audio

--- demo/index.js
+++ demo/index.js
@@ -19,22 +19,34 @@ async function getMedia(constraints) {
 
 
 async function main() {
+  var abOrig = null
+  var abProc = null
+
   const wave = new Wavecore()
   console.log(wave)
   const s = await getMedia({audio:true,video:false})
   console.log(s)
   wave.recStream(s)
+  const audioCtx = new AudioContext()
 
   document.getElementById("stop").onclick = async function () {
     s.stop()
     console.log(wave)
-    const abProc = await wave.audioBuffer({dcOffset: true, normalize: true, store: true})
+    abOrig = await wave.audioBuffer({dcOffset:false})
+    console.log(abOrig)
+    abProc = await wave.audioBuffer({dcOffset: true, normalize: true, store: true})
     console.log(abProc)
-    const audioCtx = new AudioContext()
     const s2 = audioCtx.createBufferSource()
     s2.buffer = abProc
     s2.connect(audioCtx.destination)
     s2.start()
+  }
+
+  document.getElementById("orig").onclick = async function () {
+    const s1 = audioCtx.createBufferSource()
+    s1.buffer = abOrig
+    s1.connect(audioCtx.destination)
+    s1.start()
   }
 }
 
