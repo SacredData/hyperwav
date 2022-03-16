a3564fb2e1c02839d41908dea21dfeabf7396669
agrathwohl <andrew@storyboard.fm>
2022-03-16 15:25:40 -0500

    update tests

--- test/sox.js
+++ test/sox.js
@@ -4,6 +4,7 @@ const Hypercore = require('hypercore')
 const path = require('path')
 const ram = require('random-access-memory')
 const { Readable } = require('stream')
+const Wavecore = require('../index')
 const WavecoreSox = require('../sox')
 const WaveFile = require('wavefile').WaveFile
 
@@ -11,9 +12,9 @@ const source = fs.readFileSync(path.join(__dirname, 'test.wav.raw'))
 
 describe('WavecoreSox', function () {
   describe('#stats', function () {
-      const core26 = new WavecoreSox({ source })
+      const core26 = new WavecoreSox()
     it('should get sox stats and stat output on the audio data', async function () {
-      await Promise.resolve(core26.open())
+      await Promise.resolve(core26.open({ source }))
       const statsOut = await core26.stats()
       expect(statsOut).to.not.equal(null)
     })
@@ -23,7 +24,6 @@ describe('WavecoreSox', function () {
     it('should get the max volume adjustment without clipping', async function () {
       await Promise.resolve(core27.open())
       const vol = await core27._volAdjust()
-      // expect(vol).to.equal(1.076)
       expect(vol).to.equal(1.189)
     })
   })
@@ -32,31 +32,29 @@ describe('WavecoreSox', function () {
     it('should increase gain by 2.0dBFS', async function () {
       await Promise.resolve(core31.open())
       const gainIncCore = await core31.gain(2)
-      const gainInc = WavecoreSox.fromCore(gainIncCore.core, core31)
-      const gainStats = await gainInc.stats()
-      expect(gainStats.split('\n')[3]).to.equal('Pk lev dB       0.00')
+      expect(gainIncCore).to.be.instanceof(Wavecore)
     })
   })
   describe('#tempo', function () {
     const core25 = new WavecoreSox({ source })
     it('should slow down the audio by 50%', async function () {
       await Promise.resolve(core25.open())
-      const orig = core25.core.byteLength
+      const orig = core25.byteLength
       const slowBy50 = await core25.tempo(0.5)
-      await slowBy50.core.update()
-      const slow = slowBy50.core.byteLength
+      await slowBy50.update()
+      const slow = slowBy50.byteLength
       expect(slow).to.equal(orig*2)
     })
     it('should speed up the audio by 200%', async function () {
-      const orig = core25.core.byteLength
+      const orig = core25.byteLength
       const fasterBy200 = await core25.tempo(2.0)
-      await fasterBy200.core.update()
-      const faster = fasterBy200.core.byteLength
+      await fasterBy200.update()
+      const faster = fasterBy200.byteLength
       expect(faster).to.equal(orig/2)
     })
     it('should provide stats', async function () {
       const statsTest = await core25.tempo(2.0, { stats: true })
-      expect(statsTest.core.byteLength).to.equal(2163456)
+      expect(statsTest.byteLength).to.equal(2163456)
     })
   })
   describe('#vad', function () {
@@ -64,7 +62,7 @@ describe('WavecoreSox', function () {
     it('should remove excessive silence from the recording', async function () {
       await Promise.resolve(core32.open())
       const vadCore = await core32.vad()
-      expect(vadCore.core.byteLength).to.equal(4298112)
+      expect(vadCore.byteLength).to.equal(4298112)
     })
   })
   describe('#norm', function () {
@@ -72,8 +70,7 @@ describe('WavecoreSox', function () {
     it('should normalize the audio to 0dBFS', async function () {
       await Promise.resolve(core28.open())
       const normCore = await core28.norm()
-      const norm = WavecoreSox.fromCore(normCore.core, core28)
-      const stats = await norm.stats()
+      const stats = await normCore.stats()
       expect(stats.split('\n')[3]).to.equal('Pk lev dB      -0.00')
     })
   })
